6f3370e3ded5df9e42596a817feec61fbf817b50
Derek Rhodes <physci@gmail.com>
2020-10-29 12:47:08 -0400

    just messing around with a web view onto the interpreter

--- simpleweb.lisp
+++ simpleweb.lisp
@@ -1,27 +1,60 @@
 (asdf:oos 'asdf:load-op :hunchentoot)
 
-(defpackage :testserv
-  (:use :cl :hunchentoot)
-  (:export :start-server))
+;; (defpackage :testserv
+;;   (:use :cl :hunchentoot)
+;;   (:export :start-server))
+;; (in-package :testserv)
 
-(in-package :testserv)
+(in-package #:regmach4wasm)
 
 ;; Add a simple prefix dispatcher to the *dispatch-table*
 (setq *dispatch-table*
       `(
         ,(create-prefix-dispatcher "/hello-world" 'hello-page)
+        ,(create-prefix-dispatcher "/step-emu" 'step-emu)
+        ,(create-prefix-dispatcher "/emu" 'hello-page)
         ,(create-prefix-dispatcher "/img" 'img-page)))
 
 ;; Handler functions either return generated Web pages as strings,
 ;; or write to the output stream returned by write-headers
-(defun hello-page ()
-  "<html><body>Hello Worlds!</body></html>")
+(defun step-emu ()
+  (emu-step test-emu)
+  (hello-page))
 
-(defun start-server (&key (port 4242))
+(defun hello-page ()
+  (format nil "<html><body> ~a </body></html>"
+          (regfile-html (mcvm-regfile (emu-microcode-vm test-emu)))))
+
+(defun fmt-reg (reg regfile)
+  (format nil "<td>R~2,' d | ~8,'0x</td>" reg (regfile-get-reg regfile reg)))
+
+(defun table-row (f i)
+  (apply 'concatenate 'string
+         (list  "<tr>"
+                (funcall f (+ i 0)) (funcall f (+ i 8))
+                (funcall f (+ i 16)) (funcall f (+ i 24))
+                "</tr>")))
+
+(defun regfile-html (regfile)
+  (flet ((f (r) (fmt-reg r regfile)))
+    (concatenate 'string
+                 "<table border=0 style=\"width:700px\">"                 
+                 (table-row #'f 0)
+                 (table-row #'f 1)
+                 (table-row #'f 2)
+                 (table-row #'f 3)
+                 (table-row #'f 4)
+                 (table-row #'f 5)
+                 (table-row #'f 6)
+                 (table-row #'f 7)
+                 "</table>")))
+
+(regfile-html (make-regfile))
+
+(defun start-server (&key (port 4243))
   (start (make-instance 'easy-acceptor :port port)))
 
-(testserv:start-server :port 4242)
-
+;; (regmach4wasm:start-server :port 4243)
 
 (defun img-page ()
   (setf (content-type*) "image/png")
